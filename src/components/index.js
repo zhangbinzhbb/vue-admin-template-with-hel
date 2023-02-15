dc15274a1fcee3caf49689278bca046536e387b0
zhangbinzhbb <13020106399@163.com>
2023-02-15 14:15:39 +0800

    add: v

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,9 @@
-// export { default as SvgIcon } from './SvgIcon'
-// export { default as Hamburger } from './Hamburger'
-// export { default as TestTitle } from './TestTitle'
+export { default as SvgIcon } from './SvgIcon'
+export { default as Hamburger } from './Hamburger'
+export { default as TestTitle } from './TestTitle'
 
-import TestTitle from './TestTitle/index.vue'
+// import TestTitle from './TestTitle/index.vue'
 
-export default {
-  TestTitle
-}
+// export default {
+//   TestTitle
+// }
