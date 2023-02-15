3ac09e9244c8073ecec499536c8a6a02a64f398d
zhangbinzhbb <13020106399@163.com>
2023-02-15 17:51:35 +0800

    add: 注释

--- src/main.js
+++ src/main.js
@@ -2,12 +2,11 @@ import { LIB_NAME } from './configs/subApp'
 import { libReady, isMasterApp } from 'hel-lib-proxy'
 
 async function main() {
+  // if语句保证你的项目是共享模块还是独立运行
   if (isMasterApp()) {
     await import('./loadApp')
   } else {
     const libProperties = await import('./entrance/libProperties')
-
-    console.log("======123=====")
     // 注意此处传递的是 default
     console.log('libProperties.default:', libProperties.default)
     libReady(LIB_NAME, libProperties.default)
