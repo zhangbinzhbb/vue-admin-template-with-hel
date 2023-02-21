e58115a3c6821192034d6145beb803430ac326d8
zhangbinzhbb <13020106399@163.com>
2023-02-21 15:35:50 +0800

    add: 项目初始化

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-export { default as SvgIcon } from './SvgIcon'
-export { default as Hamburger } from './Hamburger'
-export { default as TestTitle } from './TestTitle'
-
-// import TestTitle from './TestTitle/index.vue'
-
-// export default {
-//   TestTitle
-// }
